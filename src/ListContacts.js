43700d87967f35e002aa03f49cd335dc8b15ef84
EduardoZola <eduzol@gmail.com>
2017-07-13 02:52:50 -0700

    add filtered contact list

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
 
@@ -18,11 +20,23 @@ class ListContacts extends Component {
         });
      }
 
+    clearQuery = () => {
+        this.setState({ query: '' })
+    }
     render (){
+
+        let showingContacts;
+        if ( this.state.query){
+            const match = new RegExp(escapeRegExp(this.state.query),'i');
+            showingContacts = this.props.contacts.filter( (contact) => match.test(contact.name) );
+        }else{
+            showingContacts = this.props.contacts;
+        }
+        showingContacts.sort(sortBy('name'));
+
         return(
             <div className='list-contacts'> 
-                {JSON.stringify(this.state)}
-                <div className='list-contacts-top'>
+                 <div className='list-contacts-top'>
                     <input className='search-contacts'
                     type='text'
                     placeholder='Search contacts'
@@ -30,8 +44,14 @@ class ListContacts extends Component {
                     onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
+                 {showingContacts.length !== this.props.contacts.length && (
+                    <div className='showing-contacts'>
+                        <span>Now showing {showingContacts.length} of {this.props.contacts.length} total</span>
+                        <button onClick={this.clearQuery}>Show all</button>
+                    </div>
+                )}
                 <ol className='contact-list'>
-                    { this.props.contacts.map( (contact) => ( 
+                    { showingContacts.map( (contact) => ( 
                     <li key={contact.id} className="contact-list-item"> 
                         <div className="contact-avatar" style={{
                             backgroundImage : `url(${contact.avatarURL})`
